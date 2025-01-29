0d7a4d020c4f27e18fca7ea6a9e3a2eeaf0a3295
fancyguyiitb <22b3923@iitb.ac.in>
2025-01-29 17:25:26 +0530

    working on message list

---
+++ src/components/message.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Doc, Id } from "../../convex/_generated/dataModel";
+import Renderer from "./renderer";
+
+interface MessageProps {
+  id: Id<"messages">;
+  memberId: Id<"members">;
+  authorImage?: string;
+  authorName?: string;
+  isAuthor: boolean;
+  reactions: Array<
+    Omit<Doc<"reactions">, "memberId"> & {
+      count: number;
+      memberIds: Id<"members">[];
+    }
+  >;
+  body: Doc<"messages">["body"];
+  image: string | null | undefined;
+  createdAt: Doc<"messages">["_creationTime"];
+  updatedAt: Doc<"messages">["updatedAt"];
+  isEditing: boolean;
+  isCompact?: boolean;
+  setEditingId: (id: Id<"messages"> | null) => void;
+  hideThreadButton?: boolean;
+  threadCount?: number;
+  threadImage?: string;
+  threadTimestamp?: number;
+}
+
+const Message = ({
+  id,
+  memberId,
+  authorImage,
+  authorName = "Member",
+  isAuthor,
+  reactions,
+  body,
+  image,
+  createdAt,
+  updatedAt,
+  isEditing,
+  isCompact,
+  setEditingId,
+  hideThreadButton,
+  threadCount,
+  threadImage,
+  threadTimestamp,
+}: MessageProps) => {
+  return <div>
+    <Renderer value={body}/>
+  </div>;
+};
+
+export default Message;
